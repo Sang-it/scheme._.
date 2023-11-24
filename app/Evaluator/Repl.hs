340c31ce1e6224416191d0d7a71ed505e097ff42
Sangit Manandhar <rux12@icloud.com>
2023-11-23 22:08:17 -0500

    feat: add standard library

--- app/Evaluator/Repl.hs
+++ app/Evaluator/Repl.hs
@@ -1,11 +1,13 @@
 module Evaluator.Repl where
 
+import           Control.Monad.Except  (runExceptT)
+import           Data.Functor          ((<&>))
 import           Evaluator.Environment (bindVars, primitiveBindings,
                                         runIOThrows)
 import           Evaluator.Eval        (eval, evalAndPrint, ioPrimitives,
                                         primitives)
 import           Evaluator.Reader      (readPrompt)
-import           Internal              (Primitive (Atom, List, String))
+import           Internal              (Env, Primitive (Atom, List, String))
 import           System.IO             (hPutStrLn, stderr)
 
 until_ :: (Monad m) => (a -> Bool) -> m a -> (a -> m ()) -> m ()
@@ -15,10 +17,19 @@ until_ pred prompt action = do
     then return ()
     else action result >> until_ pred prompt action
 
+loadStandardLibrary :: [String] -> IO Env
+loadStandardLibrary args =
+  do
+   env <- primitiveBindings primitives ioPrimitives >>= flip bindVars [("args", List $ map String $ drop 1 args)]
+   runExceptT (eval env (List [Atom "load", String "lib/standard.scm"]))
+   return env
+
 runFile :: [String] -> IO ()
 runFile args = do
-  env <- primitiveBindings primitives ioPrimitives >>= flip bindVars [("args", List $ map String $ drop 1 args)]
+  env <- loadStandardLibrary args
   runIOThrows (show <$> eval env (List [Atom "load", String (head args)])) >>= hPutStrLn stderr
 
 runRepl :: IO ()
-runRepl = primitiveBindings primitives ioPrimitives >>= until_ (== "quit") (readPrompt "Lang > ") . evalAndPrint
+runRepl = do
+  env <- loadStandardLibrary []
+  until_ (== "quit") (readPrompt "> ") (evalAndPrint env)
